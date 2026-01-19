ed05f7723031d81bd865d099a6b4ecda7fde7ebd
nasywa07 <nasywafitrialia@gmail.com>
2026-01-19 06:11:44 +0000

    game v1.0

---
+++ game.py
@@ -0,0 +1,9 @@
+secret_number = 777
+guess_number = int(input("Masukkan Tebak Angka:"))
+
+while guess_number != secret_number:
+    print("Tebakan Salah, Silakan Coba Lagi")
+    guess_number = int(input("Masukkan Tebak Angka:"))
+
+print("Selamat... ! Tebakan Anda Benar!!")
+print("kod e ini saa buat di codespace")
